1b3625e331c6854d689d683069cf412a73776817
goncharenkoa <goncharenkoadamivna@gmail.com>
2016-05-22 18:51:58 +0300

    first test commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  
+  	$("#toggle1").click(function(){
+        $("#riskmanagement_options").toggle();
+    });
+
+    $("#toggle2").click(function(){
+        $("#affected_regions").toggle();
+    });
+
+    $("#toggle3").click(function(){
+        $("#requested_by").toggle();
+    });
+
+    $("#toggle4").click(function(){
+        $("#submitted_timerange").toggle();
+    });
+
+    $("#toggle5").click(function(){
+        $("#day_of_week").toggle();
+    });
+
+
+});
